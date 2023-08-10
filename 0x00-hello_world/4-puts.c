c19afad2e6d8197594c837a9cb3d5d0e450f397c
fadi1623 <ffzakhir@gmail.com>
2023-08-10 22:25:54 +0000

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point 
+ * 
+ * Descriptiom : A C code that prints with puts function 
+ *
+ * always return 0 (Success)
+*/
+
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }

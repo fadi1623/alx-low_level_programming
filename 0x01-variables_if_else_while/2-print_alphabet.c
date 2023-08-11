e6c3cf3d250ce0eb11f3911c714b50b88137ad93
fadi1623 <ffzakhir@gmail.com>
2023-08-11 22:48:15 +0000

    C TASKS

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descriptiom : A C code that prints from a to z
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 122; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+
+	return (0);
+}

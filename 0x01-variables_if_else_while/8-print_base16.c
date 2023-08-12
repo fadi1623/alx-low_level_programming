67452e793f8f34262c261e50b58e866557895c2c
fadi1623 <ffzakhir@gmail.com>
2023-08-12 00:44:38 +0000

    C TASKS

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descriptiom : A C code that prints from 0 to 10
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+{
+	putchar(48 + i);
+}
+	for (i = 97; i < 103; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+
+	return (0);
+}

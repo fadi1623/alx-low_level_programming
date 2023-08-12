f55a15c06971c1f1d68f9eb9f1f9dc21c8112f88
fadi1623 <ffzakhir@gmail.com>
2023-08-12 00:30:14 +0000

    C TASKS

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descriptiom : A C code that prints from z to a
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+{
+	putchar(i);
+}
+	putchar('\n');
+
+	return (0);
+}

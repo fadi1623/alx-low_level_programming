1953f6c6ab1a1ba15d84ea4299743f51e779bdf9
fadi <ffzakhir2@gmail.com>
2023-08-15 16:25:35 -0700

    heyy

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Descriptiom : A C code that checks last digit
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+	return (0);
+}

6d79a66da8bebef48ce437156c804aa280d669d4
fadi <ffzakhir2@gmail.com>
2023-08-15 15:08:28 -0700

    FUNCTIONS

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Descriptiom : A C code that checks last digit
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char arr[8] = '_putchar';
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(arr[i]);
+	}
+
+	return (0);
+}

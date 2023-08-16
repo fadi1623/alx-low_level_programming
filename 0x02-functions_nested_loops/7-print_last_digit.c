fe75496ca1be70f271187a370f4bf204fb208f96
fadi <ffzakhir2@gmail.com>
2023-08-16 14:30:02 -0700

    C TASKS

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - check if char is alphabet
+ * @c: is the char to be checked
+ * Return: always 0 .
+*/
+int print_last_digit(int c)
+{
+	c = c % 10;
+	return (c);
+}

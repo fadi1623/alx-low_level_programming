631e470dfb705f253da2c988aa5dcfea8a2b7def
fadi <ffzakhir2@gmail.com>
2023-08-16 13:53:48 -0700

    C TASKS

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - check if char is alphabet
+ * @c: is the char to be checked
+ * Return: always 0 .
+*/
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-c);
+}

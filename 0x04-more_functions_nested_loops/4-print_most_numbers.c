2e8bed2e2a3bc73b2c9ca6c28f9098251d62a712
fadi <ffzakhir2@gmail.com>
2023-08-17 15:26:48 -0700

    C TASKS

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_numbers - print numbers
+ * print_most_numbers - print most numbers
  *
  * Return: 0.
 */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 

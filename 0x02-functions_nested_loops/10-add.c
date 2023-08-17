afdee969f385bff6712f3a9b47bfe0a28e8d8ad1
fadi <ffzakhir2@gmail.com>
2023-08-16 17:46:25 -0700

    C TASKS

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add function
+ *
+ * @a: add paramater
+ * @b: add paramater
+ *
+ * Return: always c .
+*/
+int add(int a, int b)
+{
+	a = a + b;
+	_putchar('0' + a);
+}

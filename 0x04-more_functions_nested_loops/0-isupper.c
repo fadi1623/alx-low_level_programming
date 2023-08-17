11a9261a664f295ac26de19d552b3d178a5de667
fadi <ffzakhir2@gmail.com>
2023-08-17 12:02:40 -0700

    c tasks

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,9 @@
  * @c: is the char to be checked
  * Return: 1 if c is captial, 0 if not.
 */
-int _isupper(int c)
+int _isupper(char c)
 {
-	char d;
-
-	if (d >= 'A' &&  d <= 'Z')
+	if (c >= 'A' &&  c <= 'Z')
 	{
 		return (1);
 	}

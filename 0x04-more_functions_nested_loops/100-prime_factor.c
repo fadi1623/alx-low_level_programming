082b802e215f24a6e5c05a756fbbbd1389196816
fadi <ffzakhir2@gmail.com>
2023-08-18 16:12:33 -0700

    C TASKS

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,31 +1,63 @@
 #include <stdio.h>
 
 /**
-*main- prints the largest prime factor
-*of a number
-*
-*Return: returns 0
-*/
+ * _sqrt - find the square root
+ *
+ * @x: input
+ *
+ * Return: square root of x
+ */
+
+double _sqrt(double x)
+{
+	float sqrt, tmp;
+
+	sqrt = x / 2;
+	tmp = 0;
+
+	while (sqrt != tmp)
+	{
+		tmp = sqrt;
+		sqrt = (x / tmp + tmp) / 2;
+	}
+
+	return (sqrt);
+}
+
+/**
+ * lpm - finds the largest prime factor
+ *
+ * @num: input
+ */
+
+void lpm(long int num)
+{
+	int prm, largest;
+
+	while (num % 2 == 0)
+		num = num / 2;
+	for (prm = 3; prm <= _sqrt(num); prm += 2)
+	{
+		while (num % prm == 0)
+		{
+			num = num / prm;
+			largest = prm;
+		}
+	}
+	if (num > 2)
+		largest = num;
+	printf("%d\n", largest);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
-long number = 612852475143;
-int inc;
-while (inc++ < number / 2)
-{
-if (number % inc == 0)
-{
-number /= 2;
-continue;
-}
+	lpm(612852475143);
 
-for (inc = 3; inc < number / 2; inc += 2)
-{
-if (number % inc == 0)
-number /= inc;
-
-}
-}
-printf("%ld\n", number);
-return (0);
+	return (0);
 }

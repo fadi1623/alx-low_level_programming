7a6584dd05771f8972d9e0e02c2a6860fe76514a
fadi <ffzakhir2@gmail.com>
2023-08-19 16:21:16 -0700

    C TASKS

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: sum of mult of 3 and 5
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; num < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			j = i +j;
+	}
+	printf("%d\n", i);
+
+	return (0);
+}

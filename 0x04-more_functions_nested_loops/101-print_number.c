a6f75af6d36201ef8412708f249fa58025a4e4f2
fadi <ffzakhir2@gmail.com>
2023-08-20 05:30:23 -0700

    C TASKS

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,5 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-	if ((num / 10) > 0)
-		print_number(num / 10);
-	_putchar((num % 10) + 48);
+	_putchar('0' + n);
 }

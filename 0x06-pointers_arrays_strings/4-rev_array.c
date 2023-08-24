d8ec46964e33e47e1c7113ecb3dea366813159f1
root <root@DESKTOP-14T8UKI.localdomain>
2023-08-25 00:59:35 +0300

    c tasks

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - main function
+ *
+ * Description: 'reverse the elements of an array'
+ *
+ * @a: array
+ * @n: the number of the element in the array
+ */
+
+void reverse_array(int *a, int n)
+{int i;
+
+    for (i = 0; i < n / 2; i++)
+    { 
+    char temp = a[i];
+    a[i] = a[n - i - 1];
+    a[n - i - 1] = temp;
+    }
+}

5364f73dd5f885dfdc1c61a4729649d2fb6af423
fadi <ffzakhir2@gmail.com>
2023-08-24 10:29:15 -0700

    c tasks

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncpy - copies a string.
  *
@@ -9,15 +10,16 @@
  * Return: dest
 */
 char *_strncpy(char *dest, char *src, int n)
-{   int i, size = 0;
+{
+	int i;
 
-    for (i = 0; (src[i] != '\0' && i < n); i++)
-    {
-        dest[i] = src[i];
-    }
-    for (; (i < n); i++)
-        {
-            dest[i] = '\0';
-        }
-    return (dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }

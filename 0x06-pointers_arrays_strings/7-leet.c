79c4c5280a24a672e6b277fd63c483cf55da7971
fadi1623 <ffzakhir@gmail.com>
2023-08-25 01:48:28 +0300

    c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet -main function
+ *
+ * Description: 'Encodes a string to 1337
+ *
+ * @s: String to encode
+ *
+ * Return: coded string
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char letter[] = "aAeEoOtTlL";
+	char code[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letter[j] != '\0'; j++)
+		{
+			if (s[i] == letter[j])
+			{
+				s[i] = code[j];
+			}
+		}
+	}
+
+	return (s);
+}

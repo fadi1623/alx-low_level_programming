6923ed6813c55c6164a3280a74ef286b7efadfe8
fadi1623 <ffzakhir@gmail.com>
2023-08-27 17:56:17 +0300

    c tasks

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rot13 -main function
+ *
+ * Description: 'Encodes a string using rot13'
+ *
+ * @s: String to encode
+ *
+ * Return: coded string
+ */
+
+char *rot13(char *s)
+{
+    char set[] ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char change[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+    int i, j;
+     
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; set[j] != '\0'; j++)
+        {
+            if (set[j] == s[i])
+            {
+            s[i] = change[j];
+            break;
+            }
+            else
+            continue;
+        }
+
+    }
+    return (s);
+}
